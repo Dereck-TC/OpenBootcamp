53d618e78f927978b445a129b420d23479ccdcf6
Dereck-TC <alextellocastro456680@gmail.com>
2022-05-10 14:54:28 -0500

    tarea 05

---
+++ Tarea 5/ejercicio1.js
@@ -0,0 +1,9 @@
+let altura = 170
+let altmetros = 1.69
+let peso = 68.8
+let redondear_arriba = altmetros.toFixed(1)
+console.log(redondear_arriba)
+let redondear_abajo = Math.floor(peso)
+console.log(redondear_abajo)
+let maxvalor = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+console.log(maxvalor)
