61f097f2e75ebf4798b65672506c141fd2bcb51b
Dereck-TC <alextellocastro456680@gmail.com>
2022-07-22 19:57:05 -0500

    esLint

---
+++ tarea-13/tarea-ESlint.js
@@ -0,0 +1,18 @@
+let nom = "Dereck";
+let ape = 'Tello';
+let estudiante = `Estudiante: ${nom} ${ape}`;
+console.log(estudiante);
+let estudianteMayus = (`Estudiante: ${nom} ${ape}`).toUpperCase();
+console.log(estudianteMayus)
+let estudianteMin = (`Estudiante: ${nom} ${ape}`).toLowerCase();
+console.log(estudianteMin)
+let longitud = estudiante.length;
+console.log(longitud);
+let primeraletra = nom.charAt(0)
+console.log(primeraletra)
+let ultimaletra = ape.charAt(ape.length-1);
+console.log(ultimaletra)
+let sinEspacio = estudiante.replace(/ /g,"");
+console.log(sinEspacio)
+let bool = estudiante.includes(nom);
+console.log(bool)
